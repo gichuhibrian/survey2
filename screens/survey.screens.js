70fd645dbe4cc183c83d328163cf878d9f32f50e
Brian Chege <brianchegegichuhi@gmail.com>
2021-11-29 13:31:42 +0300

    Fetched Api from open trivia

--- screens/survey.screens.js
+++ screens/survey.screens.js
@@ -1,37 +1,57 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
-import {} from "react-native-web";
+import axios from "axios";
 const SurveyScreen = ({navigation}) => {
+    const [questions, setQuestions] = useState([]);
+    const [number, setNumber] = useState(0);
+    const getQuestions = async() => {
+        const response = await fetch('https://opentdb.com/api.php?amount=10&type=multiple')
+        const data = await(response.json())
+        console.log('response', data.results);
+        setQuestions(data.results);
+    }
+
+    useEffect(() => {
+       getQuestions();
+    }, []);
+
     return (
         <View style={styles.container}>
-            <View style={styles.question}>
-                <Text>This is the first question</Text>
-            </View>
-            <View style={styles.answers}>
-                <TouchableOpacity>
-                    <Text>Option 1</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>Option 2</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>Option 3</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>Option 4</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={styles.buttons}>
-                <TouchableOpacity>
-                    <Text>SKIP</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>NEXT</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('Completed')}>
-                    <Text>END</Text>
-                </TouchableOpacity>
-            </View>
+            {
+                questions && (
+                    <View style={styles.parent}>
+                        <View style={styles.question}>
+                            <Text style={styles.questionText}>Q. This is the first question</Text>
+                        </View>
+                        <View style={styles.answers}>
+                            <TouchableOpacity style={styles.answerButton}>
+                                <Text style={styles.answer}>Option 1</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.answerButton}>
+                                <Text style={styles.answer}>Option 2</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.answerButton}>
+                                <Text style={styles.answer}>Option 3</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.answerButton}>
+                                <Text style={styles.answer}>Option 4</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.footer}>
+                            <TouchableOpacity style={styles.button}>
+                                <Text style={styles.buttonText}>SKIP</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.button}>
+                                <Text style={styles.buttonText}>NEXT</Text>
+                            </TouchableOpacity>
+                            {/*<TouchableOpacity style={styles.button}>*/}
+                            {/*    <Text style={styles.buttonText}>END</Text>*/}
+                            {/*</TouchableOpacity>*/}
+                        </View>
+                    </View>
+                )
+            }
+
         </View>
     )
 }
@@ -40,10 +60,11 @@ export default SurveyScreen
 
 const styles = StyleSheet.create({
     container: {
-        padding: 12,
+        padding: 40,
+        paddingHorizontal: 20,
         height: '100%',
     },
-    buttons: {
+    footer: {
         marginBottom: 12,
         paddingVertical: 16,
         justifyContent: 'space-between',
@@ -56,4 +77,36 @@ const styles = StyleSheet.create({
         marginVertical: 16,
         flex: 1,
     },
+    button: {
+        paddingHorizontal: 16,
+        backgroundColor: '#B8B8FF',
+        padding: 20,
+        borderRadius: 16,
+        alignItems: 'center',
+        marginBottom: 30,
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#fff',
+
+    },
+    questionText: {
+        fontSize: 28,
+    },
+    answer: {
+        fontSize: 18,
+        fontWeight: '500',
+        color: 'black'
+    },
+    answerButton: {
+        paddingVertical: 12,
+        marginVertical: 6,
+        backgroundColor: '#FFD8BE',
+        paddingHorizontal: 12,
+        borderRadius: 12,
+    },
+    parent: {
+        height: '100%'
+    }
 })
